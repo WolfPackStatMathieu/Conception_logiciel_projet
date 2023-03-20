c86da7e8b5a8a8a84a9f0f0abbc33942904cbf41
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2023-03-20 17:02:15 +0100

    get all records, create record, edit record

--- app/note.py
+++ app/note.py
@@ -14,3 +14,28 @@ def get_notes(db: Session = Depends(get_db), limit: int = 10, page: int = 1, sea
         models.Note.title.contains(search)).limit(limit).offset(skip).all()
     return {'status': 'success', 'results': len(notes), 'notes': notes}
 
+
+# [...] create record
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create_note(payload: schemas.NoteBaseSchema, db: Session = Depends(get_db)):
+    new_note = models.Note(**payload.dict()) #We created an instance of the SQLAlchemy model
+    db.add(new_note) # only registers a transaction operation without communicating it to the database.
+    db.commit() #the .commit() method will be called to persist the data permanently in the database
+    db.refresh(new_note) # get an up-to-date version of the newly-created record.
+    return {"status": "success", "note": new_note}
+
+# [...] edit record
+@router.patch('/{noteId}')
+def update_note(noteId: str, payload: schemas.NoteBaseSchema, db: Session = Depends(get_db)):
+    note_query = db.query(models.Note).filter(models.Note.id == noteId)
+    db_note = note_query.first()
+
+    if not db_note:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'No note with this id: {noteId} found')
+    update_data = payload.dict(exclude_unset=True)
+    note_query.filter(models.Note.id == noteId).update(update_data,
+                                                       synchronize_session=False)
+    db.commit()
+    db.refresh(db_note)
+    return {"status": "success", "note": db_note}

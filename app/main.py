623085942ab7d0dd7cc72cb4f00cd7fdf6a5abde
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2023-03-20 16:24:29 +0100

    reboot

--- app/main.py
+++ app/main.py
@@ -1,48 +1,33 @@
-from fastapi import  FastAPI #BackgroundTasks,
-from app.controller.application_router import router
-import sqlite3
-from sqlite_utils import Database
-import datetime
 
-app = FastAPI()
-
-app.include_router(router)
-
-db = Database("database.db")
-db_conn = db.conn
-
-# Connect to SQLite database
-conn = sqlite3.connect('database.db')
-
-@app.on_event("startup")
-async def startup():
+# @app.on_event("startup")
+# async def startup():
 
 
-    # Set up a background task that runs every 60 seconds
-    async def check_objects(background_tasks: BackgroundTasks):
-        while True:
-            # Get the current datetime
-            now = datetime.datetime.now()
+#     # Set up a background task that runs every 60 seconds
+#     async def check_objects(background_tasks: BackgroundTasks):
+#         while True:
+#             # Get the current datetime
+#             now = datetime.datetime.now()
 
-            # Perform checks on your Python objects here
-            # For example, you could query the database to find all objects that have an expiration date less than or equal to the current date
-            c = conn.cursor()
-            c.execute("SELECT * FROM mytable WHERE expiration_date <= ?", (now,))
-            expired_objects = c.fetchall()
+#             # Perform checks on your Python objects here
+#             # For example, you could query the database to find all objects that have an expiration date less than or equal to the current date
+#             c = conn.cursor()
+#             c.execute("SELECT * FROM mytable WHERE expiration_date <= ?", (now,))
+#             expired_objects = c.fetchall()
 
-            # Do something with the expired objects, such as delete them from the database
-            for obj in expired_objects:
-                # Delete the object
-                c.execute("DELETE FROM mytable WHERE id = ?", (obj['id'],))
-                conn.commit()
+#             # Do something with the expired objects, such as delete them from the database
+#             for obj in expired_objects:
+#                 # Delete the object
+#                 c.execute("DELETE FROM mytable WHERE id = ?", (obj['id'],))
+#                 conn.commit()
 
-            # Wait for 60 seconds before checking again
-            await asyncio.sleep(60)
+#             # Wait for 60 seconds before checking again
+#             await asyncio.sleep(60)
 
-    # Start the background task
-    app.background_tasks.add_task(check_objects)
+#     # Start the background task
+#     app.background_tasks.add_task(check_objects)
 
-@app.on_event("shutdown")
-async def shutdown():
-    # Close the database connection when the app shuts down
-    conn.close()
+# @app.on_event("shutdown")
+# async def shutdown():
+#     # Close the database connection when the app shuts down
+#     conn.close()

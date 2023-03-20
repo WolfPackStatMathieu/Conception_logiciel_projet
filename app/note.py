0a4ecd44ce37765c16f8fdbfdf95fcfef161eb79
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2023-03-20 16:52:12 +0100

    Get All Records

--- app/note.py
+++ app/note.py
@@ -4,3 +4,13 @@ from fastapi import Depends, HTTPException, status, APIRouter, Response
 from .database import get_db
 
 router = APIRouter()
+
+# [...] get all records
+@router.get('/')
+def get_notes(db: Session = Depends(get_db), limit: int = 10, page: int = 1, search: str = ''):
+    skip = (page - 1) * limit
+
+    notes = db.query(models.Note).filter(
+        models.Note.title.contains(search)).limit(limit).offset(skip).all()
+    return {'status': 'success', 'results': len(notes), 'notes': notes}
+

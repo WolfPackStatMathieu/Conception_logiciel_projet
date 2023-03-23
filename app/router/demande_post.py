effc08ecb2cb03d310e0734b663856f6225820bc
Mathieu Thomassin <mathieu.thomassin@eleve.ensai.fr>
2023-03-23 23:45:12 +0100

    création DemandeModel

--- app/router/demande_post.py
+++ app/router/demande_post.py
@@ -1,16 +1,26 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-
+import datetime as dt
+from typing import Optional
 
 router = APIRouter(
     prefix='/demande',
     tags=['demande']
 )
 class DemandeModel(BaseModel):
-    pass
+    destinataire: str
+    expediteur: str
+    sujet: str
+    message: str
+    mois: int
+    jour: int
+    heure: int
+    minutes: int
+    id: int
+
 
 
 @router.post('/',
              summary='crée une demande d\'envoi de mail')
 def create_demande(demande: DemandeModel):
-    return "ok"
+    return {'data': demande}
